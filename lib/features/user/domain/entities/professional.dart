bbb2bd8fe750e9d85bbb4bd6e8e17cdf0622d39c
WorfZeBard <jexis34worf@gmail.com>
2025-10-01 17:32:10 +0800

    Add professional.dart

---
+++ lib/features/user/domain/entities/professional.dart
@@ -0,0 +1,17 @@
+// entities/professional.dart
+import 'package:equatable/equatable.dart';
+
+class Professional extends Equatable {
+  final String id;
+  final String businessName;
+  final String? logoUrl;
+
+  const Professional({
+    required this.id,
+    required this.businessName,
+    this.logoUrl,
+  });
+
+  @override
+  List<Object?> get props => [id, businessName, logoUrl];
+}
