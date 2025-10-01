cd0fc9e6cd44168c4f8ccbf1729494398f4b04b7
WorfZeBard <jexis34worf@gmail.com>
2025-10-01 21:54:52 +0800

    Add business_hours function

---
+++ lib/features/user/domain/usecases/set_business_hours.dart
@@ -0,0 +1,15 @@
+// usecases/set_business_hours.dart
+import 'package:dartz/dartz.dart';
+import '../entities/business_hours.dart';
+import '../repositories/business_hours_repository.dart';
+import '../../../core/error/failures.dart';
+
+class SetBusinessHours {
+  final BusinessHoursRepository repository;
+
+  SetBusinessHours(this.repository);
+
+  Future<Either<Failure, BusinessHours>> call(BusinessHours hours) async {
+    return await repository.setBusinessHours(hours);
+  }
+}
