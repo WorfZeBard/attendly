37beb1f3b8554759e6c58660afcbc82021357558
WorfZeBard <jexis34worf@gmail.com>
2025-10-02 07:47:15 +0800

    Add booking feature

---
+++ lib/features/user/domain/entities/booking.dart
@@ -0,0 +1,19 @@
+// entities/booking.dart
+import 'package:equatable/equatable.dart';
+
+class Booking extends Equatable {
+  final String id;
+  final String clientId;
+  final String professionalId;
+  final DateTime dateTime;
+
+  const Booking({
+    required this.id,
+    required this.clientId,
+    required this.professionalId,
+    required this.dateTime,
+  });
+
+  @override
+  List<Object?> get props => [id, clientId, professionalId, dateTime];
+}
